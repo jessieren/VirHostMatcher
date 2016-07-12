80fcfe642897e9d3798c17d231f7ca29c47a8c57
jessieren <renj@usc.edu>
2016-07-12 12:49:02 -0700

    filename only first 25 letters; hostTaxa.txt not a must

--- vhm.py
+++ vhm.py
@@ -23,15 +23,15 @@ parser.add_option("-d", "--d2star", action = "store", type = "string", dest = "o
 
 (options, args) = parser.parse_args()
 if (options.virusFaDir is None or
-		options.hostFaDir is None or
-		options.hostTaxaFile is None) :
-	#sys.stderr.write(prog_base + ": error: missing required command-line argument")
+		options.hostFaDir is None) :
+	sys.stderr.write(prog_base + ": error: missing required command-line argument")
 	parser.print_help()
 	sys.exit(0)
 
 
+nameLen = 25
 
-#################### 0: preparatio ############################
+#################### 0: preparation ############################
 ## path to the programs
 vhmPath  = os.path.dirname(sys.argv[0])
 
@@ -44,27 +44,44 @@ countKmerCpp = os.path.join(vhmPath, "countKmer.cpp")
 countKmerOut = os.path.join(vhmPath, "countKmer.out")
 #print countKmerCpp
 
-if not os.path.exists(countKmerOut) and os.path.exists(countKmerCpp) :
+#if not os.path.exists(countKmerOut) and os.path.exists(countKmerCpp) :
+#	os.system("g++ " + countKmerCpp + " -o " + countKmerOut)
+#elif not os.path.exists(countKmerCpp) :
+#	sys.stderr.write( "can't find countKmer.cpp file in " + vhmPath + "\n")
+#	sys.exit(0)
+if os.path.exists(countKmerCpp) :
 	os.system("g++ " + countKmerCpp + " -o " + countKmerOut)
-elif not os.path.exists(countKmerCpp) :
+else :
 	sys.stderr.write( "can't find countKmer.cpp file in " + vhmPath + "\n")
 	sys.exit(0)
 
+
 computeMeasureCpp = os.path.join(vhmPath, "computeMeasure.cpp")
 computeMeasureOut = os.path.join(vhmPath, "computeMeasure.out")
-if not os.path.exists(computeMeasureOut) and os.path.exists(computeMeasureCpp) :
-  os.system("g++ " + computeMeasureCpp + " -o " + computeMeasureOut)
-elif not os.path.exists(computeMeasureCpp) :
+#if not os.path.exists(computeMeasureOut) and os.path.exists(computeMeasureCpp) :
+#  os.system("g++ " + computeMeasureCpp + " -o " + computeMeasureOut)
+#elif not os.path.exists(computeMeasureCpp) :
+#	sys.stderr.write( "can't find computeMeasure.cpp file in " + vhmPath + "\n")
+#	sys.exit(0)
+if os.path.exists(computeMeasureCpp) :
+	os.system("g++ " + computeMeasureCpp + " -o " + computeMeasureOut)
+else :
 	sys.stderr.write( "can't find computeMeasure.cpp file in " + vhmPath + "\n")
 	sys.exit(0)
 
 computed2starCpp = os.path.join(vhmPath, "computeMeasure_onlyd2star.cpp")
 computed2starOut = os.path.join(vhmPath, "computeMeasure_onlyd2star.out")
-if not os.path.exists(computed2starOut) and os.path.exists(computed2starCpp) :
+#if not os.path.exists(computed2starOut) and os.path.exists(computed2starCpp) :
+#	os.system("g++ " + computed2starCpp + " -o " + computed2starOut)
+#elif not os.path.exists(computed2starCpp) :
+#	sys.stderr.write( "can't find computeMeasure_onlyd2star.cpp file in " + vhmPath + "\n")
+#	sys.exit(0)
+if os.path.exists(computed2starCpp) :
 	os.system("g++ " + computed2starCpp + " -o " + computed2starOut)
-elif not os.path.exists(computed2starCpp) :
+else :
 	sys.stderr.write( "can't find computeMeasure_onlyd2star.cpp file in " + vhmPath + "\n")
 	sys.exit(0)
+
 
 if int(options.onlyD2star) == 1 :
 	computeMeasureOut=computed2starOut
@@ -73,15 +90,15 @@ else :
 
 ## tmp file directory
 if not os.path.exists(options.outDir) :
-	os.mkdir(options.outDir)
+	os.makedirs(options.outDir)
 tmpDir = os.path.join(options.outDir, "tmp")
 if not os.path.exists(tmpDir) :
-	os.mkdir(tmpDir)
+	os.makedirs(tmpDir)
 
 ## kmer count directory
 kmerCountPath = os.path.join(tmpDir, "kmerCount")
 if not os.path.exists(kmerCountPath) :
-	os.mkdir(kmerCountPath)
+	os.makedirs(kmerCountPath)
 
 ## virusFaList, hostFaList
 virusFaList = os.listdir(options.virusFaDir)
@@ -102,24 +119,53 @@ hostListFileWrite = open(hostListFile, 'a')
 
 #time.sleep(6) # delays for 10 seconds
 
+#################### 00: if hostTaxa missing ###################
+hostTaxaFile = os.path.join(options.outDir, "hostTaxa.txt")
+hostTaxaFileWrite = open(hostTaxaFile, 'w') ## make file blank
+hostTaxaFileWrite.close()
+hostTaxaFileWrite = open(hostTaxaFile, 'a')
+
+hostTaxaFileWrite.write("hostNCBIName	hostName	hostSuperkingdom	hostPhylum	hostClass	hostOrder	hostFamily	hostGenus	hostSpecies\n")
+for currentFileName in hostFaList :
+	if currentFileName.startswith('.') :
+		continue
+	if len(currentFileName) > nameLen :
+		sys.stdout.write( " the file name has more than 25 letters! Use the first 25 letters as the name \n")
+		currentFileNameS = currentFileName[:nameLen]
+	else :
+		currentFileNameS = currentFileName
+	hostTaxaFileWrite.write(currentFileNameS)
+	for i in range(1,9) :
+		hostTaxaFileWrite.write("\t" + "unknown")
+	hostTaxaFileWrite.write("\n")
+hostTaxaFileWrite.close()
+
+options.hostTaxaFile = hostTaxaFile
+
+
 
 #################### 1: count kmer and prepare list files ############################
 #sys.stdout.write("Step 1: counting kmers \n")
 for currentFileName in virusFaList :
 	if currentFileName.startswith('.') :
 		continue
-	sys.stdout.write("Step 1: counting kmers for virus " + currentFileName + "\n")
+	if len(currentFileName) > nameLen :
+		currentFileNameS = currentFileName[:nameLen]
+	else :
+		currentFileNameS = currentFileName
+	sys.stdout.write("Step 1: counting kmers for virus " + currentFileNameS + "\n")
 	for w in range(1, (kmax+1)) :
 		currentFilePath = os.path.join(options.virusFaDir, currentFileName)
-		currentKmerCountPath = os.path.join(kmerCountPath, currentFileName)
+		currentKmerCountPath = os.path.join(kmerCountPath, currentFileNameS)
 		cmdKmer = countKmerOut + " -l -k " + str(w) + \
 										" -i " + currentFilePath +\
-										" -o " + currentKmerCountPath
+										" -o " + currentKmerCountPath +\
+										" -s " + currentFileNameS
 		cmdKmerOut = subprocess.Popen(cmdKmer, shell=True, \
 																			stderr = subprocess.PIPE, \
 																			stdout = subprocess.PIPE)
 		cmdKmerOut.wait()
-	virusListFileWrite.write(currentFileName + " " + \
+	virusListFileWrite.write(currentFileNameS + " " + \
 													 currentKmerCountPath + " " +\
 														str(2) + "\n")
 virusListFileWrite.close()
@@ -128,18 +174,24 @@ virusListFileWrite.close()
 for currentFileName in hostFaList :
 	if currentFileName.startswith('.') :
 		continue
-	sys.stdout.write("Step 1: counting kmers for host " + currentFileName + "\n")
+	if len(currentFileName) > nameLen :
+		currentFileNameS = currentFileName[:nameLen]
+	else :
+		currentFileNameS = currentFileName
+	sys.stdout.write("Step 1: counting kmers for host " + currentFileNameS + "\n")
 	for w in range(1, (kmax+1)) :
 		currentFilePath = os.path.join(options.hostFaDir, currentFileName)
-		currentKmerCountPath = os.path.join(kmerCountPath, currentFileName)
+		currentKmerCountPath = os.path.join(kmerCountPath, currentFileNameS)
 		cmdKmer = countKmerOut + " -l -k " + str(w) + \
 							" -i " + currentFilePath +\
-							" -o " + currentKmerCountPath
+							" -o " + currentKmerCountPath +\
+							" -s " + currentFileNameS
+		#print cmdKmer
 		cmdKmerOut = subprocess.Popen(cmdKmer, shell=True, \
 																			stderr = subprocess.PIPE, \
 																			stdout = subprocess.PIPE)
 		cmdKmerOut.wait()
-	hostListFileWrite.write(currentFileName + " " + \
+	hostListFileWrite.write(currentFileNameS + " " + \
 												 currentKmerCountPath + " " +\
 												 str(2) + "\n")
 hostListFileWrite.close()
@@ -152,6 +204,7 @@ hostListFileWrite.close()
 sys.stdout.write("Step 2: compute distance/dissimialrity measures \n")
 cmdCptMeasure = computeMeasureOut + " -k " + str(kmax) + \
 								" -i " + virusListFile + " -j " + hostListFile + " -o " + options.outDir + " -t " + options.hostTaxaFile
+#print cmdCptMeasure
 #print cmdCptMeasure
 cmdCptMeasureOut = subprocess.Popen(cmdCptMeasure, shell=True, \
 														 stderr = subprocess.PIPE, \
